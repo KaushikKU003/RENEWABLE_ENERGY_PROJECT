3ab6bba278177bd31bc5f143794700869c46b3d3
babith1234 <you@example.com>
2024-02-25 20:40:07 +0530

    finance controller done

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const adminRouter = require("./controllers/adminController");
 const projectRouter = require('./controllers/projectController')
+const financeRouter = require("./controllers/financeController")
 
 const app = express();
 
@@ -9,6 +10,7 @@ app.use(express.json());
 
 app.use("/app/admin", adminRouter);
 app.use("/app/project",projectRouter)
+app.use("/app/finance",financeRouter)
 
 app.listen(4000, () => {
   console.log(`Server running successfully on port 4000`);
