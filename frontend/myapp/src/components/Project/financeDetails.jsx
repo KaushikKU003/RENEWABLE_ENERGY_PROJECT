bb8fe40512f96c8a343eb11393a0cd23a911a69b
KaushikKU003 <kaushikuacharya@gmail.com>
2024-03-05 21:52:16 +0530

    front-end all insertion completed

--- frontend/myapp/src/components/Project/financeDetails.jsx
+++ frontend/myapp/src/components/Project/financeDetails.jsx
@@ -8,7 +8,7 @@ const FinanceDetails = () => {
     total_cost: '',
     funding_source: '',
     revenue_generation: '',
-    project_returns: ''
+    return_on_investment: ''
   });
   const handleSuccess = (msg) =>
   toast.success(msg, {
@@ -39,7 +39,13 @@ const FinanceDetails = () => {
     console.log(financeDetails);
     localStorage.setItem('financeDetails', JSON.stringify(financeDetails));
   };
-
+  const NavigationFront=()=>{
+    const projectId=localStorage.getItem("project_id")
+    navigate(`/benifitsdetails/${projectId}`)
+  }
+  const NavigationBack=()=>{
+    navigate(`/projectsdetails/`)
+  }
   return (
     <>
        <ProjectsLayout/>
@@ -49,24 +55,24 @@ const FinanceDetails = () => {
       <form class='max-w-[75%] m-auto ' onSubmit={handleSubmit}>
       <div>
         <label htmlFor="total_cost" className="block text-gray-700 text-sm font-bold mb-2">Project Total Cost:</label>
-        <input type="text" id="total_cost" name="total_cost" value={financeDetails.total_cost} onChange={handleInputChange} className="shadow appearance-none border rounded w-full py-2 px-3 mb-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" />
+        <input type="number" id="total_cost" name="total_cost" value={financeDetails.total_cost} onChange={handleInputChange} className="shadow appearance-none border rounded w-full py-2 px-3 mb-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" />
       </div>
       <div>
         <label htmlFor="funding_source" className="block text-gray-700 text-sm font-bold mb-2">Project Funding Source:</label>
-        <input type="number" id="funding_source" name="funding_source" value={financeDetails.funding_source} onChange={handleInputChange} className="shadow appearance-none border rounded w-full py-2 px-3 mb-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" />
+        <input type="text" id="funding_source" name="funding_source" value={financeDetails.funding_source} onChange={handleInputChange} className="shadow appearance-none border rounded w-full py-2 px-3 mb-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" />
       </div>
       <div>
         <label htmlFor="revenue_generation" className="block text-gray-700 text-sm font-bold mb-2">Project Revenue Generation:</label>
         <input type="number" id="revenue_generation" name="revenue_generation" value={financeDetails.revenue_generation}  onChange={handleInputChange}className="shadow appearance-none border rounded w-full py-2 px-3 mb-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" />
       </div>
       <div>
-        <label htmlFor="project_returns" className="block text-gray-700 text-sm font-bold mb-2">Return Of Investement:</label>
-        <input type="number" id="project_returns" name="project_returns" value={financeDetails.project_returns} onChange={handleInputChange} className="shadow appearance-none border rounded w-full py-2 px-3 mb-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"  />
+        <label htmlFor="return_on_investment" className="block text-gray-700 text-sm font-bold mb-2">Return Of Investement (%):</label>
+        <input type="number" id="return_on_investment" name="return_on_investment" value={financeDetails.return_on_investment} onChange={handleInputChange} className="shadow appearance-none border rounded w-full py-2 px-3 mb-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"  />
       </div>
       <div className='flex justify-evenly'>
-      <button type="submit" className="mt-4 bg-indigo-600 text-white py-2 px-4 rounded hover:bg-indigo-700" onClick={() => navigate('/projectsdetails')}>Back</button>
+      <button type="submit" className="mt-4 bg-indigo-600 text-white py-2 px-4 rounded hover:bg-indigo-700" onClick={NavigationBack}>Back</button>
       <button type="submit" className="mt-4 bg-indigo-600 text-white py-2 px-4 rounded hover:bg-indigo-700 " >Save</button>
-      <button type="submit" className="mt-4 bg-indigo-600 text-white py-2 px-4 rounded hover:bg-indigo-700 " onClick={() => navigate('/benifitsdetails')}>Next</button>
+      <button type="submit" className="mt-4 bg-indigo-600 text-white py-2 px-4 rounded hover:bg-indigo-700 " onClick={NavigationFront}>Next</button>
       </div>    
       </form>
       <ToastContainer />
--- frontend/myapp/src/components/Project/projectRisks.jsx
+++ frontend/myapp/src/components/Project/projectRisks.jsx
@@ -5,28 +5,47 @@ import { ToastContainer, toast } from "react-toastify";
 const ProjectRisks = () => {
     const [risks, setRisks] = useState({
         impact: '',
-        likelihood: '',
-        riskdetails:''
+        Likelihood: '',
+        risk_description:''
       });
       const navigate = useNavigate();
+      const typeMappings1 = {
+        type1: 'Critical',
+        type2: 'High',
+        type3: 'Moderate',
+        type4: 'Low',
+        // Add more mappings if needed
+      };
+      const typeMappings2={
+        type1:'High',
+        type2:'Moderate',
+        type3:'Low'
+      }
       const handleSuccess = (msg) =>
       toast.success(msg, {
         position: "top-right",
       });
       useEffect(() => {
-        const storedData = JSON.parse(localStorage.getItem('riskDetails'));
+        const storedData = JSON.parse(localStorage.getItem('riskDetails_actual'));
         if (storedData) {
             setRisks(storedData);
         }
       }, []);
     const handleSubmit=(e)=>{
         e.preventDefault();
+        const mappedType1 = typeMappings1[risks.impact];
+        const mappedType2 = typeMappings2[risks.Likelihood];
     // Save the form data
+    localStorage.setItem('riskDetails_actual', JSON.stringify(risks));
+    localStorage.setItem('riskDetails', JSON.stringify({
+      impact:mappedType1,
+      Likelihood:mappedType2,
+      risk_description:risks.risk_description
+    }));
     handleSuccess("Project Risk Details Saved")
     setTimeout(() => {
     }, 500)
     console.log(risks);
-    localStorage.setItem('riskDetails', JSON.stringify(risks));
     }
     const handleInputChange=(e)=>{
         const { name, value } = e.target;
@@ -34,6 +53,14 @@ const ProjectRisks = () => {
           ...prevState,
           [name]: value
         }));
+    }
+    const NavigationFront=()=>{
+      const projectId=localStorage.getItem("project_id")
+      navigate(`/locationdetails/${projectId}`)
+    }
+    const NavigationBack=()=>{
+      const projectId=localStorage.getItem("project_id")
+      navigate(`/benifitsdetails/${projectId}`)
     }
     return ( 
         <>
@@ -44,20 +71,33 @@ const ProjectRisks = () => {
       <form class='max-w-[75%] m-auto ' onSubmit={handleSubmit}>
       <div>
         <label htmlFor="impact" className="block text-gray-700 text-sm font-bold mb-2">Environmental Impact on Project:</label>
-        <input type="text" id="impact" name="impact" value={risks.impact} onChange={handleInputChange} className="shadow appearance-none border rounded w-full py-2 px-3 mb-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" />
+        {/* <input type="text" id="impact" name="impact" value={risks.impact} onChange={handleInputChange} className="shadow appearance-none border rounded w-full py-2 px-3 mb-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" /> */}
+        <select id="impact" name="impact" value={risks.impact} onChange={handleInputChange} className="shadow appearance-none border rounded w-full py-2 px-3 mb-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
+          <option value="">Select Impact Type</option>
+          <option value="type1">Critical Impact</option>
+          <option value="type2">High Impact</option>
+          <option value="type3">Moderate Impact</option>
+          <option value="type4">Low Impact</option>
+        </select>
       </div>
       <div>
-        <label htmlFor="likelihood" className="block text-gray-700 text-sm font-bold mb-2">Risks Liklihood:</label>
-        <input type="text" id="likelihood" name="likelihood" value={risks.likelihood} onChange={handleInputChange} className="shadow appearance-none border rounded w-full py-2 px-3 mb-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" />
+        <label htmlFor="Likelihood" className="block text-gray-700 text-sm font-bold mb-2">Risks Liklihood:</label>
+        {/* <input type="text" id="Likelihood" name="Likelihood" value={risks.Likelihood} onChange={handleInputChange} className="shadow appearance-none border rounded w-full py-2 px-3 mb-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" /> */}
+        <select id="Likelihood" name="Likelihood" value={risks.Likelihood} onChange={handleInputChange} className="shadow appearance-none border rounded w-full py-2 px-3 mb-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
+          <option value="">Select Likelihood Type</option>
+          <option value="type1">High</option>
+          <option value="type2">Moderate </option>
+          <option value="type3">Low </option>
+        </select>
       </div>
       <div>
-        <label htmlFor="riskdetails" className="block text-gray-700 text-sm font-bold mb-2">Other Risk Details:</label>
-        <input type="text" id="riskdetails" name="riskdetails" value={risks.riskdetails} onChange={handleInputChange} className="shadow appearance-none border rounded w-full py-2 px-3 mb-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" />
+        <label htmlFor="risk_description" className="block text-gray-700 text-sm font-bold mb-2">Other Risk Description:</label>
+        <input type="text" id="risk_description" name="risk_description" value={risks.risk_description} onChange={handleInputChange} className="shadow appearance-none border rounded w-full py-2 px-3 mb-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" />
       </div>
       <div className='flex justify-evenly'>
-      <button type="submit" className="mt-4 bg-indigo-600 text-white py-2 px-4 rounded hover:bg-indigo-700" onClick={() => navigate('/benifitsdetails')}>Back</button>
+      <button type="submit" className="mt-4 bg-indigo-600 text-white py-2 px-4 rounded hover:bg-indigo-700" onClick={NavigationBack}>Back</button>
       <button type="submit" className="mt-4 bg-indigo-600 text-white py-2 px-4 rounded hover:bg-indigo-700 " >Save</button>
-      <button type="submit" className="mt-4 bg-indigo-600 text-white py-2 px-4 rounded hover:bg-indigo-700 " onClick={() => navigate('/locationdetails')}>Next</button>
+      <button type="submit" className="mt-4 bg-indigo-600 text-white py-2 px-4 rounded hover:bg-indigo-700 " onClick={NavigationFront}>Next</button>
       </div>    
       </form>
       <ToastContainer />
